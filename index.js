1bf28c250949752b32a46d566107529e040ff487
silvano-paulino <silvanocavimbi20@gmail.com>
2024-11-06 15:48:11 +0100

    save note

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 const express = require("express");
+const {saveNote} = require("./db");
+const crypto = require("crypto");
 const app =  express();
 
-app.use(express.static("public"))
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname+"index.html");
 })
 
 app.get("/note/:id", (req, res) => {
-
     res.sendFile(__dirname+ "/public/note.html");
 })
+
+app.post("/notes", async (req, res) => {
+    const {content} = req.body;
+
+    if (!content) {
+        res.send("<span>Erro inesperado</span>")
+    }
+
+    const id = crypto.randomUUID();
+
+    await saveNote(id, content)
+    res.send(`
+        <p>
+            Compartilhe sua nota através do link.
+            <br/>
+            <span>${req.headers.origin}/note/${id}</span>
+        </p>
+    `);
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {console.log(`server is running on port ${PORT}`)})
